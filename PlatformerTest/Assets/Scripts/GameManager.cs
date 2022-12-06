0e9189e485cb9247807f35e3360c3772a2ea8d50
Trev <daeteri360@gmail.com>
2022-12-05 19:59:33 -0500

    High scores are stored in a txt file

--- PlatformerTest/Assets/Scripts/GameManager.cs
+++ PlatformerTest/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class GameManager : MonoBehaviour
 
     public TMP_Text HUDTimerUI;
     public TMP_Text levelCompleteUI;
+    public HighScoreManager highScoreManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,22 +37,34 @@ public class GameManager : MonoBehaviour
         EndLevelScreen();
     }
 
+    public void SwitchToTitleScreen()
+    {
+        SceneManager.LoadScene("Assets/Scenes/TitleScreen.unity");
+    }
+
     public void EndLevelScreen()
     {
         HUDTimerUI.gameObject.SetActive(false);
 
         TMP_Text YourTime = null;
+        TMP_Text BestTime = null;
         foreach (TMP_Text textComponent in levelCompleteUI.GetComponentsInChildren<TMP_Text>())
         {
             if (textComponent.name == "YourTime")
             {
                 YourTime = textComponent;
-                break;
+            }
+            if (textComponent.name == "BestTime")
+            {
+                BestTime = textComponent;
             }
         }
+        float roundedTime = Mathf.Round(timer * 100f) / 100f;
 
-        YourTime.text = "Your Time: " + Mathf.Round(timer * 100f) / 100f;
+        YourTime.text = "Your Time: " + roundedTime.ToString();
+        BestTime.text = "Best Time: " + highScoreManager.scores[0].ToString();
 
+        highScoreManager.SubmitNewScore(roundedTime.ToString());
         levelCompleteUI.gameObject.SetActive(true);
     }
 }
--- PlatformerTest/Assets/Scripts/HighScoreManager.cs
+++ PlatformerTest/Assets/Scripts/HighScoreManager.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using TMPro;
+using System;
 
 public class HighScoreManager : MonoBehaviour
 {
+    public List<float> scores;
+    private string filename = "Highscore.txt";
     // Start is called before the first frame update
     void Start()
     {
-        
+        scores = new List<float>();
+        string[] fileStrings = File.ReadAllLines(Application.dataPath + "/" + filename);
+        foreach (string s in fileStrings)
+        {
+            scores.Add(float.Parse(s));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public void SubmitNewScore(string newScore)
     {
-        
+        int numOfScores = 3;
+        float newScoreInt = float.Parse(newScore);
+
+        for (int scoreIterator = 0; scoreIterator < numOfScores; scoreIterator++)
+        {
+            if (newScoreInt < scores[scoreIterator])
+            {
+                scores.RemoveAt(numOfScores - 1);
+                scores.Add(newScoreInt);
+                scores.Sort();
+                break;
+            }
+        }
+        ///////////
+        //foreach (int s in scores) {
+        //    print(s);
+        //}
+
+        string[] fileStrings = new string[3];
+        for (int scoreIterator = 0; scoreIterator < numOfScores; scoreIterator++)
+        {
+            fileStrings[scoreIterator] = scores[scoreIterator].ToString();
+        }
+        File.WriteAllLines(Application.dataPath + "/" + filename, fileStrings);
     }
 }
