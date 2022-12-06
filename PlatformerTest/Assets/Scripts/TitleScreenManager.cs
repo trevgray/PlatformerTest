4533a95dc82eab927706837aadb692bbe6e6f866
Trev <daeteri360@gmail.com>
2022-12-05 20:16:42 -0500

    Added Highscore Screen

--- PlatformerTest/Assets/Scripts/TitleScreenManager.cs
+++ PlatformerTest/Assets/Scripts/TitleScreenManager.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using static Cinemachine.DocumentationSortingAttribute;
 
 public class TitleScreenManager : MonoBehaviour
 {
+    public HighScoreManager highScoreManager;
+    public TMP_Text titleScreen;
+    public TMP_Text highScoreScreen;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Assets/Scenes/Level1.unity");
     }
-    
+
+    public void Highscores()
+    {
+        titleScreen.gameObject.SetActive(false);
+
+        int textIterator = 0;
+        foreach (TMP_Text textComponent in highScoreScreen.GetComponentsInChildren<TMP_Text>())
+        {
+            if (textComponent.name != "Highscore")
+            {
+                textComponent.text = highScoreManager.scores[textIterator].ToString() + "s";
+                textIterator++;
+            }
+        }
+        highScoreScreen.gameObject.SetActive(true);
+    }
 }
